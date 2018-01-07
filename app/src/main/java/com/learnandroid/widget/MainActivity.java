657e579d826a7f2b8c30c36dd9775f117f0096d8
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-07 19:36:43 +0700

    Add widget

--- app/src/main/java/com/learnandroid/widget/MainActivity.java
+++ app/src/main/java/com/learnandroid/widget/MainActivity.java
@@ -2,12 +2,20 @@ package com.learnandroid.widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.gambar);
+        Glide.with(this).load("https://yhscriminalrecord.com/wp-content/uploads/2017/05/aaa.png").into(imageView);
     }
 }
